453376a5e907527772683bbe9c9e44457c81a247
dnanto <dnegron2@gmu.edu>
2020-03-01 17:35:12 -0500

    re-adding taxid
    convenient

--- sh/feature.sh
+++ sh/feature.sh
@@ -6,10 +6,10 @@ entry="${2:-/dev/stdin}"
 root="$(dirname "$0")"
 
 { 
-  printf "accver\ttitle\tsubtype\tsubname\n"; 
+  printf "accver\ttitle\ttaxid\tsubtype\tsubname\n";
   "$root"/../py/eutil.py "$entry" -eutil esummary -param retmode=json | \
     tee "$prefix.json" | \
-    jq -r '.result | del(.uids) | map([.accessionversion, .title, .subtype, .subname] | @tsv) | .[]' 
+    jq -r '.result | del(.uids) | map([.accessionversion, .title, .taxid, .subtype, .subname] | @tsv) | .[]';
 } | \
   "$root"/../R/subtype.R - subtype subname 2> /dev/null | \
   "$root"/../R/lubridate.R - collection_date 2> /dev/null > "$prefix.tsv"
