de91cbb9fe63f45151d068cda4e02f06c5dbe28c
dnanto <dnegron2@gmu.edu>
2020-03-24 16:41:05 -0400

    Update beast.sh
    no need to cd

--- scripts/beast.sh
+++ scripts/beast.sh
@@ -6,9 +6,7 @@ limit=${2:-0.5}
 find out/*/beast -type f -name "*.xml" | \
 while read -r ele; 
 do 
-    root="$(dirname $ele)";
     name="$(basename "$ele")";
-    cd "$root" || exit
-    echo "time beast -overwrite "$ele" && time treeannotator -heights mean -burnin "$burnin" -limit "$limit" "${name/.xml/.trees}" "${name/.xml/.mcc.tree}"" | \
-        qsub -V -d "$root" -N "${name//.xml}" -o "${name/.xml/.out.log}" -e "${name/.xml/.err.log}" -l nodes=1:ppn=32,walltime=48:00:00
+    echo "time beast -overwrite "$name" && time treeannotator -heights mean -burnin "$burnin" -limit "$limit" "${name/.xml/.trees}" "${name/.xml/.mcc.tree}"" | \
+        qsub -V -d "$(dirname $ele)" -N "${name//.xml}" -o "${name/.xml/.out.log}" -e "${name/.xml/.err.log}" -l nodes=1:ppn=32,walltime=48:00:00
 done;
